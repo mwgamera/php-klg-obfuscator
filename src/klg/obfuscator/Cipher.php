f098048db91425e5d92c0378fbfcef203fca5c89
mwgamera <mwgamera@gmail.com>
2012-09-25 12:07:02 +0200

    Initial import.

---
+++ src/klg/obfuscator/Cipher.php
@@ -0,0 +1,64 @@
+<?php
+namespace klg\obfuscator;
+
+/**
+ * Cipher used to obfuscate textual data.
+ * Plaintext is NULL-terminated string of bytes.
+ * Cryptogram should contain only characters that require no escaping
+ * when used in URL, SGML/XML attribute, or Javascript string literal.
+ * @uses XXTEA
+ **/
+class Cipher {
+  /**
+   * Encrypt block of data with given key.
+   * @param   string    block of data
+   * @param   integer[] 128-bit key
+   * @param   integer   initialization vector
+   * @return  string    encrypted data
+   **/
+  public static function encrypt($str, $key, $iv = null) {
+    static $rand = null;
+    if (!$rand)
+      $rand = new \klg\random\SecureRandom;
+    // convert to array
+    $str = rtrim($str, "\0"); // normalize
+    $words = array_values(unpack('N*', $str . "\0\0\0"));
+    // prepend initialization vector
+    $iv = $iv ?: $rand->get_integer();
+    array_unshift($words, $iv ?: $rand->get_integer());
+    // append random length padding
+    $rand->php_reseed();
+    for ($i = 0; mt_rand(0,1) && $i < 5; $i++)
+      array_push($words, 0);
+    $words = XXTEA::encrypt($words, $key);
+    // encode with base64url
+    array_unshift($words, 'N*');
+    $str = call_user_func_array('pack', $words);
+    $str = base64_encode($str);
+    $str = str_replace(
+      array('+','/','='),
+      array('-','_',''), $str);
+    return $str;
+  }
+
+  /**
+   * Decrypt block of data with given key.
+   * @param   string    block of data
+   * @param   integer[] 128-bit key
+   * @return  string    decrypted data
+   **/
+  public static function decrypt($str, $key) {
+    // decode base64url
+    $str = str_replace(array('-','_'), array('+','/'), $str);
+    $str = base64_decode($str);
+    $words = array_values(unpack('N*', $str));
+    // decrypt
+    $words = XXTEA::decrypt($words, $key);
+    array_shift($words); // drop IV
+    // convert to string
+    array_unshift($words, 'N*');
+    $str = call_user_func_array('pack', $words);
+    return rtrim($str, "\0");
+  }
+}
+?>
---
+++ test/klg/obfuscator/CipherTest.php
@@ -0,0 +1,59 @@
+<?php
+use klg\obfuscator\Cipher;
+
+class CipherTest extends \PHPUnit_Framework_TestCase {
+
+  /** Number of cases provided by randomProvider  */
+  const TEST_RANDOM = 64;
+
+  /**
+   * Test invertibility of encryption and syntax of cryptogram.
+   * @dataProvider randomProvider
+   **/
+  public function testInverse($msg, $key, $iv) {
+    $x = Cipher::encrypt($msg, $key, $iv);
+    $this->assertRegExp('/^[^!#$&\'()*+,\/:;=?@\[\]]+$/', $x);
+    $y = Cipher::decrypt($x, $key);
+    $this->assertEquals($msg, $y);
+  }
+
+  /**
+   * Test variability of cryptogram.
+   * @dataProvider randomProvider
+   **/
+  public function testVar($msg, $key, $iv) {
+    $x1 = Cipher::encrypt($msg, $key);
+    $x2 = Cipher::encrypt($msg, $key);
+    $x3 = Cipher::encrypt($msg, $key, $iv);
+    $key = array($iv, $iv, $iv, $iv);
+    $x4 = Cipher::encrypt($msg, $key, $iv);
+    // almost certainly shall be distinct
+    $x = array($x1=>1, $x2=>1, $x3=>1, $x4=>1);
+    $this->assertTrue(count($x) === 4);
+  }
+
+  /**
+   * Random provider.
+   **/
+  public function randomProvider() {
+    static $data = null;
+    if ($data)
+      return $data;
+    $data = array();
+    for ($i = 0; $i < self::TEST_RANDOM; $i++) {
+      $key = array();
+      $msg = array();
+      $iv = mt_rand();
+      for ($k = 0; $k < 4; $k++)
+        $key[$k] = mt_rand(0, 0xffffffff);
+      $len = 1 + mt_rand(1, 256);
+      $msg[0] = 'C*';
+      for ($k = 1; $k < $len; $k++)
+        $msg[$k] = mt_rand(1, 0xff); // not zero!
+      $msg = call_user_func_array('pack', $msg);
+      $data[] = array($msg, $key, $iv);
+    }
+    return $data;
+  }
+}
+?>
